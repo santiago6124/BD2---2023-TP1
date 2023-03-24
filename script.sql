617761f1f604a6dda8c48f4c725aee06d4ccce92
santiago6124 <71385552+santiago6124@users.noreply.github.com>
2023-03-24 18:06:53 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -78,3 +78,91 @@ CREATE TABLE ContactoCliente (
     FOREIGN KEY (contacto_id) REFERENCES Contacto(id),
     FOREIGN KEY (cliente_id) REFERENCES Cliente(id)
 );
+
+
+-- Inserts para la tabla Peluquero
+INSERT INTO Peluquero (id, nombre, apellido) VALUES
+(1, 'Juan', 'Pérez'),
+(2, 'María', 'García'),
+(3, 'Pedro', 'Sánchez'),
+(4, 'Laura', 'López'),
+(5, 'Carlos', 'Fernández');
+
+-- Inserts para la tabla Estado
+INSERT INTO Estado (id, estado, descripcion) VALUES
+(1, 'Agendada', 'La cita ha sido agendada.'),
+(2, 'Confirmada', 'La cita ha sido confirmada.'),
+(3, 'Cancelada', 'La cita ha sido cancelada.'),
+(4, 'Completada', 'La cita ha sido completada.');
+
+-- Inserts para la tabla Sucursal
+INSERT INTO Sucursal (id, nombre, direccion, telefono) VALUES
+(1, 'Sucursal A', 'Av. Principal 123', '+1 123-456-7890'),
+(2, 'Sucursal B', 'Calle Secundaria 456', '+1 234-567-8901'),
+(3, 'Sucursal C', 'Plaza Principal 789', '+1 345-678-9012'),
+(4, 'Sucursal D', 'Avenida Secundaria 012', '+1 456-789-0123'),
+(5, 'Sucursal E', 'Callejón Oscuro 345', '+1 567-890-1234');
+
+-- Inserts para la tabla Cliente
+INSERT INTO Cliente (id, nombre, apellido) VALUES
+(1, 'Luis', 'González'),
+(2, 'Ana', 'Martínez'),
+(3, 'Roberto', 'Jiménez'),
+(4, 'Carla', 'Ruiz'),
+(5, 'Mario', 'Castillo');
+
+-- Inserts para la tabla Estilo
+INSERT INTO Estilo (id, nombre, descripcion) VALUES
+(1, 'Corte clásico', 'Corte de cabello tradicional.'),
+(2, 'Corte moderno', 'Corte de cabello a la moda.'),
+(3, 'Peinado formal', 'Peinado elegante para ocasiones especiales.'),
+(4, 'Peinado casual', 'Peinado informal para el día a día.'),
+(5, 'Barba y bigote', 'Recorte y cuidado de la barba y el bigote.');
+
+-- Inserts para la tabla Contacto
+INSERT INTO Contacto (id, mail, numeroTelefono) VALUES
+(1, 'juan@example.com', '+1 123-456-7890'),
+(2, 'maria@example.com', '+1 234-567-8901'),
+(3, 'pedro@example.com', '+1 345-678-9012'),
+(4, 'laura@example.com', '+1 456-789-0123'),
+(5, 'carlos@example.com', '+1 567-890-1234');
+
+-- Inserts para la tabla Cita
+INSERT INTO Cita (id, FechaHora, Peluquero_id, cliente_id, sucursal_id, estado_id) VALUES
+(1, '2023-03-24 09:00:00', 1, 1, 1, 1),
+(2, '2023-03-25 10:00:00', 2, 2, 2, 1),
+(3, '2023-03-26 11:00:00', 3, 3, 3, 1),
+(4, '2023-03-27 12:00:00', 4, 4, 4, 1),
+(5, '2023-03-28 13:00:00', 5, 5, 5, 1);
+
+-- Inserts para la tabla Servicio
+INSERT INTO Servicio (id, nombre, precio, descripcion) VALUES
+(1, 'Corte de cabello', 200, 'Corte de cabello básico.'),
+(2, 'Peinado', 150, 'Peinado sencillo.'),
+(3, 'Tinte de cabello', 300, 'Tinte para cabello de cualquier color.'),
+(4, 'Afeitado', 100, 'Afeitado clásico.'),
+(5, 'Tratamiento capilar', 500, 'Tratamiento para hidratar y fortalecer el cabello.');
+
+-- Inserts para la tabla CitaServicio
+INSERT INTO CitaServicio (id, cita_id, servicio_id) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4),
+(5, 5, 5);
+
+-- Inserts para la tabla EstiloCliente
+INSERT INTO EstiloCliente (id, estilo_id, cliente_id) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4),
+(5, 5, 5);
+
+-- Inserts para la tabla ContactoCliente
+INSERT INTO ContactoCliente (id, contacto_id, cliente_id) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4),
+(5, 5, 5);
